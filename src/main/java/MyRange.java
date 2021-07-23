81a130ffd69472e5f545ff593d372b386c03da83
hattaya.tra <hattaya.tra@ngernturbo.com>
2021-07-23 15:16:58 +0700

    add test case #2

--- src/main/java/MyRange.java
+++ src/main/java/MyRange.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class MyRange {
+public class MyRange {
+    public static final int ASCII_VALUE_OF_1 = 48;
+    private final String input;
+
+    public MyRange(String input) {
+        this.input = input;
+    }
+
+    public boolean isStartWithInclude() {
+        return this.input.startsWith("[");
+    }
+
+    public boolean isEndWithInclude() {
+        return this.input.endsWith("]");
+    }
+
+    public int getStart() {
+        if(isStartWithInclude()) {
+            return this.input.charAt(1) - ASCII_VALUE_OF_1;
+//        return Integer.parseInt(String.valueOf(this.input.charAt(1)));
+        }
+        else {
+            return this.input.charAt(1) - 47;
+        }
+    }
+
+    public int getEnd() {
+        if(isEndWithInclude()) {
+            return this.input.charAt(3) - 48;
+//        return Integer.parseInt(String.valueOf(this.input.charAt(3)));
+        }
+        else {
+            return this.input.charAt(3) - 49;
+        }
+    }
+
 }
--- src/test/java/MyRangeTest.java
+++ src/test/java/MyRangeTest.java
@@ -1,2 +1,72 @@
-package PACKAGE_NAME;public class MyRangeTest {
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyRangeTest {
+
+    @Test
+    @DisplayName("ข้อมูล [1,5] นั้นจะขึ้นต้นด้วย [ ผลที่ได้คือ 1 ")
+    public void startNumberWithInclude() {
+        MyRange myRange = new MyRange("[1,5]");
+        int result = myRange.getStart();
+        assertEquals(1, result);
+    }
+
+    @Test
+    @DisplayName("ข้อมูล (1,5] นั้นจะขึ้นต้นด้วย ( ผลที่ได้คือ 2 ")
+    public void startNumberWithExclude() {
+        MyRange myRange = new MyRange("(1,5]");
+        int result = myRange.getStart();
+        assertEquals(2, result);
+    }
+
+    @Test
+    @DisplayName("ข้อมูล [1,5] นั้นจะลงท้ายด้วย ( ผลที่ได้คือ 5 ")
+    public void EndNumberWithInclude() {
+        MyRange myRange = new MyRange("[1,5]");
+        int result = myRange.getEnd();
+        assertEquals(5, result);
+    }
+
+    @Test
+    @DisplayName("ข้อมูล [1,5) นั้นจะลงท้ายด้วย ) ผลที่ได้คือ 4 ")
+    public void EndNumberWithExclude() {
+        MyRange myRange = new MyRange("[1,5)");
+        int result = myRange.getEnd();
+        assertEquals(4, result);
+    }
+
+    @Test
+    @DisplayName("ข้อมูล (1,5] นั้นจะขึ้นต้นด้วย ( ผลที่ได้คือ false")
+    public void startWithExclude() {
+        MyRange myRange = new MyRange("(1,5]");
+        boolean result = myRange.isStartWithInclude();
+        assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("ข้อมูล [1,5] นั้นจะขึ้นต้นด้วย [ (include) ผลที่ได้คือ true")
+    public void startWithInclude() {
+        MyRange myRange = new MyRange("[1,5]");
+        boolean result = myRange.isStartWithInclude();
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("ข้อมูล [1,5] นั้นจะลงท้ายด้วย ] (include) ผลที่ได้คือ true")
+    public void endWithInclude() {
+        MyRange myRange = new MyRange("[1,5]");
+        boolean result = myRange.isEndWithInclude();
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("ข้อมูล [1,5) นั้นจะลงท้ายด้วย ] (include) ผลที่ได้คือ false")
+    public void endWithExclude() {
+        MyRange myRange = new MyRange("[1,5)");
+        boolean result = myRange.isEndWithInclude();
+        assertFalse(result);
+    }
+
 }
